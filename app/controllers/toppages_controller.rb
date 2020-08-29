eb40dbace38268d08f9c12fe7008cd593005516f
flaminig.marty <flaming.marty@gmail.com>
2020-08-29 13:19:34 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])   # 一覧表示用 用
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])   # 一覧表示用 用
     end
   end
 end
